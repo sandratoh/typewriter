83a84daa0b34854ac22576d8bb16dc46081a3773
Sandra To <sandra.to.e@gmail.com>
2021-02-09 22:25:13 +0000

    New line added after output, alternate c style loop also added

--- typewriter.js
+++ typewriter.js
@@ -5,24 +5,25 @@ let delay = 0;
 for (const char of sentence) {
   delay += 50;
 
-  const lastChar = sentence[sentence.length - 1];
-  // console.log(lastChar);
   setTimeout(() => {
     process.stdout.write(char);
   }, delay);
-
-
-
-  // setTimeout(() => {
-  //   if (char !== sentence[sentence.length - 1]) {
-  //     process.stdout.write(char);
-  //   } else {
-  //     process.stdout.write(char + '\n');
-  //   }
-  // }, delay);
-
-
 }
 
+setTimeout(() => {
+  process.stdout.write('\n');
+}, delay);
 
 
+// Alt method: C Style loop and set new line as condition inside loop
+// for (let i = 0; i < sentence.length; i++) {
+//   delay += 50;
+
+//   setTimeout(() => {
+//     if (i !== sentence.length - 1) {
+//       process.stdout.write(sentence[i]);
+//     } else {
+//       process.stdout.write(sentence[i] + '\n');
+//     }
+//   }, delay);
+// }
